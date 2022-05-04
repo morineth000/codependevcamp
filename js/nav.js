867911975a34bcf80f007b3909196948fda3cf30
morineth000 <98051910+morineth000@users.noreply.github.com>
2022-05-04 13:33:53 -0600

    nav in process

--- js/nav.js
+++ js/nav.js
@@ -1,6 +1,6 @@
 const toggleButton = document.getElementsByClassName('toggle-button')[0]
-const navbarlinks = document.getElementsByClassName('links')[0]
+const navbarLinks = document.getElementsByClassName('navbar-links')[0]
 
 toggleButton.addEventListener('click', () => {
-    navbarlinks.classList.toggle('active')
-}) 
+    navbarLinks.classList.toggle('active')
+});
